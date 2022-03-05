448d8fadcd8e67118ac1fadf4a61be71edd0588a
Stone Comstock <comstockstone@yahoo.com>
2022-03-05 13:07:03 -0800

    created schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS movies_db;
+CREATE DATABASE movies_db;
+-- Create table for movie names
+CREATE TABLE movies (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    movie_name VARCHAR(100) NOT NULL
+);
+-- Create table for movie reviews, linking movie_id column to movies(id) on movies table.
+CREATE TABLE reviews (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    movie_id INT,
+    REVIEW TEXT,
+    FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE SET NULL
+)
